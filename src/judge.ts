069818bf19fa9c314c2898a6d25741e39ddafe0e
kinew <git@kinew.wtf>
2024-06-25 00:26:56 +0100

    removed AI nonsense and replaced with even/odd check on ascii value of one DID character

--- src/judge.ts
+++ src/judge.ts
@@ -1,16 +1,9 @@
-import { createCanvas, loadImage } from "canvas";
-import fs from "node:fs/promises";
-import { generateText, tool } from "ai";
-import { openai } from "@ai-sdk/openai";
-import { z } from "zod";
+// Edited to remove all the AI nonsense.
+// All enquiries or incidents contact stopai@kinew.wtf
+
 import { did, getAgent } from "./agent.js";
 
 export const judge = async (subject: string) => {
-  const avatar = `avatars/${subject}.png`;
-
-  // skip if avatar already exists
-  if (await fs.stat(avatar).catch(() => null))
-    throw new Error("Avatar already judged");
 
   const agent = await getAgent();
   const profile = await agent.getProfile({ actor: subject });
@@ -20,42 +13,16 @@ export const judge = async (subject: string) => {
   if (profile.data.labels?.some((label) => label.src === did))
     throw new Error("Already judged");
 
-  if (!profile.data.avatar) throw new Error("No avatar");
 
-  const image = await loadImage(profile.data.avatar);
-  const canvas = createCanvas(100, 100);
-  const ctx = canvas.getContext("2d");
-  ctx.drawImage(image, 0, 0, 100, 100);
-  await fs.writeFile(avatar, canvas.toBuffer());
+  if (subject.charCodeAt(17) % 2 === 0) {
+     const answer = new String("kiki");
+   	   }
+  else {
+     const answer = new String("bouba");
+     	   }
 
-  await generateText({
-    model: openai("gpt-4o"),
-    messages: [
-      {
-        role: "user",
-        content: [
-          {
-            type: "text",
-            text: `Is ${profile.data.displayName || profile.data.handle} (@${
-              profile.data.handle
-            }) kiki or bouba? Bouba = round, soft, and curvy. Kiki = sharp, jagged, and angular.`,
-          },
-          {
-            type: "image",
-            image: canvas.toBuffer(),
-          },
-        ],
-      },
-    ],
-    toolChoice: "required",
-    tools: {
-      decide: tool({
-        parameters: z.object({
-          answer: z.union([z.literal("kiki"), z.literal("bouba")]),
-        }),
-        execute: async ({ answer }) => {
-          console.log(`@${profile.data.handle} is ${answer}`);
-          await agent
+  await console.log(`@${profile.data.handle} is ${answer}`);		
+  await agent	
             .withProxy("atproto_labeler", did)
             .api.tools.ozone.moderation.emitEvent({
               event: {
@@ -71,8 +38,4 @@ export const judge = async (subject: string) => {
               createdAt: new Date().toISOString(),
               subjectBlobCids: [],
             });
-        },
-      }),
-    },
-  });
 };

3bdb3a0610655dfed4458d3470fe6fe8048dd142
Samuel Newman <mozzius@protonmail.com>
2024-09-21 16:40:12 +0100

    undo acciental commit of video/gif counting

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,47 @@
-import { AppBskyEmbedExternal, AppBskyEmbedVideo } from "@atproto/api";
-
+import { did } from "./agent.js";
+import { judge, removeLabel } from "./judge.js";
 import { Jetstream } from "@skyware/jetstream";
 
+import fs from "node:fs";
+
+let interval: ReturnType<typeof setInterval>;
+const cursorFile = fs.readFileSync("cursor.txt", "utf8");
+if (cursorFile) console.log(`Initiate firehose at cursor ${cursorFile}`);
+else console.log("Initiate firehose (no cursor)");
 
 const jetstream = new Jetstream({
-  wantedCollections: [ 'app.bsky.feed.post'],
+  wantedCollections: ["app.bsky.feed.like"],
+  cursor: cursorFile || undefined,
+});
 
+jetstream.on("open", () => {
+  interval = setInterval(() => {
+    const cursor = jetstream.cursor;
+    if (cursor) {
+      console.log(`${new Date().toISOString()}: ${cursor}`);
+      fs.writeFile("cursor.txt", cursor.toString(), (err) => {
+        if (err) console.log(err);
+      });
+    }
+  }, 60000);
 });
 
 jetstream.on("error", (err) => console.error(err));
 
+jetstream.on("close", () => clearInterval(interval));
 
-
-
-let video = 0
-let gif = 0
-
-setInterval(() => {
-  console.log(`Videos: ${video}, Gifs: ${gif}`);
-}, 5000)
-
-jetstream.onCreate("app.bsky.feed.post", (op) => {
-  const embed = op.commit.record.embed
-  if (AppBskyEmbedVideo.isMain(embed)) {
-    video++
-  } else if (AppBskyEmbedExternal.isMain(embed)) {
-    if (embed.external.uri.includes('media.tenor.com') && embed.external.uri.includes('hh=') && embed.external.uri.includes('ww=')) {
-      gif++
-    }
+jetstream.onCreate("app.bsky.feed.like", (op) => {
+  if (
+    op.commit.record?.subject?.uri ===
+    `at://${did}/app.bsky.labeler.service/self`
+  ) {
+    judge(op.did).catch((err) => console.error(err.message));
+  } else if (
+    op.commit.record?.subject?.uri ===
+    `at://${did}/app.bsky.feed.post/3l4obq2dxhs2p`
+  ) {
+    removeLabel(op.did).catch((err) => console.error(err.message));
   }
-
-})
+});
 
 jetstream.start();
